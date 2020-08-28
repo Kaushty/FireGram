752486f4953e7dedbc8a18abf5d2bd7d404fe422
kaushty <kaushikshetty4898@gmail.com>
2020-08-29 00:28:57 +0530

    feat: create useFirestore hook to access firestore collection.
    created a custom hook useFirestore to access and fetch the images from
    the collection.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
 import Title from './comp/Title'
 import UploadForm from './comp/UploadForm';
+import ImageGrid from './comp/ImageGrid'
 
 function App() {
   return (
     <div>
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
